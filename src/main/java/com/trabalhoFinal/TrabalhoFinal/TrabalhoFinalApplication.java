cdee1cf14734bcf26d9df4c70b68cfaf8add3da1
Dio <2024020011@aluno.osorio.ifrs.edu.br>
2026-06-25 22:27:40 -0300

    Update TrabalhoFinalApplication.java
    izi

--- src/main/java/com/trabalhoFinal/TrabalhoFinal/TrabalhoFinalApplication.java
+++ src/main/java/com/trabalhoFinal/TrabalhoFinal/TrabalhoFinalApplication.java
@@ -2,10 +2,16 @@ package com.trabalhoFinal.TrabalhoFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrabalhoFinalApplication {
+public class TrabalhoFinalApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TrabalhoFinalApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhoFinalApplication.class, args);
 	}
